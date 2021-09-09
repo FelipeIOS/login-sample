b15c34d880f0f81fe324e8b4d127a82ffef2b795
Felipe Miranda <>
2021-09-09 19:33:27 -0300

    create project

---
+++ login-sampleTests/login_sampleTests.swift
@@ -0,0 +1,33 @@
+//
+//  login_sampleTests.swift
+//  login-sampleTests
+//
+//  Created by Felipe Miranda on 09/09/21.
+//
+
+import XCTest
+@testable import login_sample
+
+class login_sampleTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
