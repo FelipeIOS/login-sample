1e00513d17f830810f3083751c1c2a197eea6139
Felipe Miranda <>
2021-09-09 21:06:23 -0300

    implementei a troca de cor da home

--- login-sample/LoginVC.swift
+++ login-sample/LoginVC.swift
@@ -7,10 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.view.backgroundColor = .blue
         // Do any additional setup after loading the view.
     }
 
